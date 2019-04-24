2cdb6e57affc0e8fa468b3991b808b15f3dcf43c
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2019-04-24 17:43:08 -0400

    Q learning tuning

--- Code/setup.py
+++ Code/setup.py
@@ -31,17 +31,20 @@ dt = 5/60
 
 # Central Solution
 log_central, cost_central = ess.run_central_solution(K, raw_data.LMP, ene_cap, ene_init, power_ch, power_dis, eff_ch, eff_dis, dt)
+print("Central Cost:", cost_central)
 
 # Random Solution
 log_random, cost_random = ess.run_random_solution(K, raw_data.LMP, ene_cap, ene_init, power_ch, power_dis, eff_ch, eff_dis, dt)
+print("Random Cost:", cost_random)
 
 # Q Learning Solution
-alpha = 0.5
-gamma = 0.5
+alpha = 0.4
+gamma = 0.2
 epsilon = 0.8
 eta = 0.2
 log_q, cost_q = ess.run_q_solution(K, raw_data.LMP, ene_cap, ene_init, power_ch, power_dis, eff_ch, eff_dis, dt,
                                    epsilon, alpha, gamma, eta)
+print("Q Cost:", cost_q)
 
 
 # summary
